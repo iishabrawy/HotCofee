9d7a1147f4186788ac2b881ab232399de1d35036
Ismael AlShabrawy <micro0@live.com>
2020-06-02 01:23:09 +0200

    first commit

---
+++ HotCofee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AddOrderViewController.swift
+//  HotCofee
+//
+//  Created by Ismael AlShabrawy on 6/8/19.
+//  Copyright © 2019 Ismael AlShabrawy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- HotCofee/Controllers/OrdersTableViewController.swift
+++ HotCofee/Controllers/OrdersTableViewController.swift
@@ -1,20 +1,18 @@
 //
-//  ViewController.swift
+//  OrdersTableViewController.swift
 //  HotCofee
 //
 //  Created by Ismael AlShabrawy on 6/8/19.
 //  Copyright © 2019 Ismael AlShabrawy. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class OrdersTableViewController: UITableViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
